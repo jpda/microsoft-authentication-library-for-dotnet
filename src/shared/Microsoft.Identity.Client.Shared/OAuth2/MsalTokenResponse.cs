41efd7130aa92217ab18a736d5417b1f900671ce
Bogdan Gavril <bogavril@microsoft.com>
2020-01-19 19:49:55 +0000

    remove ns

--- src/shared/Microsoft.Identity.Client.Shared/OAuth2/MsalTokenResponse.cs
+++ src/shared/Microsoft.Identity.Client.Shared/OAuth2/MsalTokenResponse.cs
@@ -1,11 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Microsoft.Identity.Client.Utils;
 using System;
-using System.Collections.Generic;
-using System.Globalization;
-using Microsoft.Identity.Client.Internal.Broker;
 using Microsoft.Identity.Json;
 
 namespace Microsoft.Identity.Client.OAuth2
